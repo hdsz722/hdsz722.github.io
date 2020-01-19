610e6fd2d3008eacbc05cf79ebb3093c80a0159a
ctaylor CIT230 <hdsz722@byui.edu>
2020-01-18 15:13:13 -0900

    commit

--- js/myscript.js
+++ js/myscript.js
@@ -7,6 +7,6 @@
    let oLastModif = new Date(document.lastModified);
    document.getElementById("lastupdated").textContent = oLastModif;
 
-   let d = new Date():
+   let d = new Date();
    let oYear = d.getFullYear();
-   document.getElementById("copyrite");
+   document.getElementById("copyrite").textContent = oYear;
