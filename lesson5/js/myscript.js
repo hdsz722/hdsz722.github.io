afd344cb455d6a176fb40530fe5e3cf573af1a58
ctaylor CIT230 <hdsz722@byui.edu>
2020-02-05 16:45:19 -0900

    commit

---
+++ lesson5/js/myscript.js
@@ -0,0 +1,21 @@
+/* Defining Table
+* Input: A person's name
+* Processing: Create a verse of scripture for that person
+* Output: The Scripture
+*/
+
+   var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+   var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+   let d = new Date();
+   let oYear = d.getFullYear();
+   let oCurrentDate = days[d.getDay()] + ", " + d.getDate() + " " + months[d.getMonth()] + " " + d.getFullYear();
+   document.getElementById("todaydate").textContent = oCurrentDate;
+   document.getElementById("copyrite").textContent = oYear;
+
+   const hambutton = document.querySelector (".ham");
+   hambutton.addEventListener ("click", toggleMenu, false);
+
+   function toggleMenu() {
+      document.querySelector(".design-doc-menu").classList.toggle("responsive");
+   }
