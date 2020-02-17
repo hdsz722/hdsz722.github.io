5ba73a3f7e6539495cc5315c544e4207d17e8b2e
ctaylor CIT230 <hdsz722@byui.edu>
2020-02-17 09:14:07 -0900

    commit

---
+++ lazyloading/js/myscript.js
@@ -0,0 +1,27 @@
+const images = document.querySelectorAll("src");
+
+function preloadImage(img) {
+  const src=img.getAttribute("src");
+  if(!src) {return;}
+  
+  img.src=src;
+}
+const imgOptions = {
+  threshold: 1,
+  rootMargin: "0px 0px -300px 0px"
+};
+
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+  entries.forEach(entry => {
+    if (!entry.isIntersection) {
+      return;
+    } else {
+      preloadImage(entry.target);
+      imgObserver.unobserve(entry.target);
+    }
+  })
+}, imgOptions);
+
+images.forEach(image => {
+  imgObserver.observe(image);
+} );
