be55c97ece2a463fb85febdff193ad08c8d2266a
ctaylor CIT230 <hdsz722@byui.edu>
2020-02-19 21:28:34 -0900

    commit

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -21,40 +21,14 @@ if('IntersectionObserver' in window) {
   });
 }
 
+const imgOptions = {
+  threshold: 1,
+  rootMargin: "0px 0px 50px 0px"
+};
+
 const loadImages = (image) => {
   image.setAttribute('src', image.getAttribute('data-src'));
   image.onload = () => {
       image.removeAttribute('data-src');
   };
 };
-
-
-
-/*const images = document.querySelectorAll('img[data-src]');
-  function preloadImage(img) {
-  const src=img.getAttribute("src");
-  if(!src) {return;}
-  
-  img.src = src;
-}
-
-const imgOptions = {
-  threshold: 1,
-  rootMargin: "0px 0px -300px 0px"
-};
-
-const imgObserver = new IntersectionObserver(function(entries, imgObserver) {
-  entries.forEach(entry => {
-    if (!entry.isIntersecting) {
-      return;
-    } else {
-        console.log(entry.target);
-        preloadImage(entry.target);
-        imgObserver.unobserve(entry.target); 
-    }
-  })
-}, imgOptions);
-
-images.forEach(image => {
-  imgObserver.observe(image);
-}); */
