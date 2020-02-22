7d22ce6caa185509a6c92be5ecdce5ea5239a61f
ctaylor CIT230 <hdsz722@byui.edu>
2020-02-22 14:28:18 -0900

    commit

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -3,7 +3,7 @@ let imagesToLoad = document.querySelectorAll('img[data-src');
 
 const imgOptions = {
   threshold: 0,
-  rootMargin: '0px 0px 50px 0px'
+  rootMargin: '0px 0px 200px 0px'
 };
 
 if('IntersectionObserver' in window) {
