6d8859c2503ffa4b8f7670a6be9094d8bf3f9880
ctaylor CIT230 <hdsz722@byui.edu>
2020-03-04 18:00:00 -0900

    commit

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -11,11 +11,19 @@ fetch(requestURL)
         for (let i=0; i <prophets.length; i++) {
             let card = document.createElement('section');
             let h2 = document.createElement('h2');
+            let h3 = document.createElement('h3');          
+            let h4 = document.createElement('h4');
+            let img = document.createElement('img');
 
-        
             h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-        
+            h3.textContent = 'Date of Birth: ' + prophets[i].birthdate;
+            h4.textContent = 'Place of Birth: ' + prophets[i].birthplace;
+//            img = image.setAttribute('src', prophets[i].imageurl);
+
             card.appendChild(h2);
+            card.appendChild(h3);
+            card.appendChild(h4);
+            card.appendChild(img);
         
             document.querySelector('div.cards').appendChild(card);
         };
