5fc8f87424ef77538eb81c48be56265f77810806
ctaylor CIT230 <hdsz722@byui.edu>
2020-03-04 20:36:13 -0900

    commit

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -6,27 +6,31 @@ fetch(requestURL)
     })
     .then(function(jsonObject) {
         const prophets = jsonObject['prophets'];
-
+ 
         console.table(jsonObject); //temporary checking for valid response and data parsing
-        for (let i=0; i <prophets.length; i++) {
+        for (let i = 0; i < prophets.length; i++ ) {        
+//        }); (let i=0; i <prophets.length; i++) {
             let card = document.createElement('section');
             let h2 = document.createElement('h2');
             let h3 = document.createElement('h3');          
             let h4 = document.createElement('h4');
-            let img = document.createElement('img');
+            let image = document.createElement('img');
 
-            h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+            let fullname = prophets[i].name + ' ' + prophets[i].lastname;
+
+            h2.textContent = fullname;
             h3.textContent = 'Date of Birth: ' + prophets[i].birthdate;
-            h4.textContent = 'Place of Birth: ' + prophets[i].birthplace;
-//            img = image.setAttribute('src', prophets[i].imageurl);
-
+            h4.textContent = 'Place of Birth: '+ prophets[i].birthplace;
+            image.setAttribute('src', prophets[i].imageurl);
+            image.setAttribute('alt', 'portrait of ' + fullname);
+        
             card.appendChild(h2);
             card.appendChild(h3);
             card.appendChild(h4);
-            card.appendChild(img);
-        
+            card.appendChild(image);
+         
             document.querySelector('div.cards').appendChild(card);
-        };
+        }
     });
 
 
