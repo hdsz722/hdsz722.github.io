eea3da235db8e17b0ca923d779ae8db7fd85732f
ctaylor CIT230 <hdsz722@byui.edu>
2020-03-07 20:17:27 -0900

    commit

--- lesson9/js/towndata.js
+++ lesson9/js/towndata.js
@@ -15,22 +15,27 @@ fetch(requestURL)
                 case 'Preston': case 'Fish Haven': case 'Soda Springs':
                     let card=document.createElement('section');
                     let townName=document.createElement('h2');
-                    let townMoto=document.createElement('h3');
+                    let townMotto=document.createElement('h5');
                     let townFounded=document.createElement('p');
                     let townPopulation=document.createElement('p');
                     let townRainfall=document.createElement('p');
                     let image=document.createElement('img');
         
                     townName.textContent = town.name;
-                    townMoto.textContent = town.moto;
+                    townName.setAttribute('class', "card-title");
+                    townMotto.textContent = town.motto;
+                    townMotto.setAttribute('class', "card-motto");
                     townFounded.textContent = 'Year Founded: ' + town.yearFounded;
+                    townFounded.setAttribute('class', "card-text");
                     townPopulation.textContent = 'Population: ' + town.currentPopulation;
+                    townPopulation.setAttribute('class', "card-text");
                     townRainfall.textContent = 'Annual Rain Fall: ' + town.averageRainfall;
+                    townRainfall.setAttribute('class', "card-text");
                     image.setAttribute('src', 'images/' + town.photo);
                     image.setAttribute('alt', 'photo of ' + town.name);
         
                     card.appendChild(townName);
-                    card.appendChild(townMoto);
+                    card.appendChild(townMotto);
                     card.appendChild(townFounded);
                     card.appendChild(townPopulation);
                     card.appendChild(townRainfall);
