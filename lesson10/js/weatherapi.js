cce3bb538daaa6e2799de9cd0d0235c969fa35da
ctaylor CIT230 <hdsz722@byui.edu>
2020-03-11 22:21:08 -0800

    commit

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -4,25 +4,30 @@ const townName = document.getElementById("town").textContent;
 var townId="0";
 
 switch (townName){
-    case'Preston, Idaho':
+    case `Preston, Idaho`:
         townId = "5604473";
         break;
-    case 'Soda Springs, Idaho':
+    case `Soda Springs, Idaho`:
         break;
-    case 'Fish Haven, Idaho':
+    case `Fish Haven, Idaho`:
         break;
     default:
         townId="5604473";
         break;
 }
 
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?id={"+townId+"}&appid={"+openWeatherKey+"}"
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id="+townId+"&units=imperial&APPID="+openWeatherKey
 fetch(apiURL)
-//    .then ((response)=> response.jason())
-    .then(function(response) {
-        return response.json();
-    })    
+    .then ((response)=> response.json())
     .then ((jsObject) => {
 //        console.table(jsonObject); //temporary checking for valid response and data parsingv
         console.log(jsObject);
+        const currentTemp = document.querySelector(`#current-temp`);
+        currentTemp.textContent = jsObject.main.temp;
+
+        const imagesrc = `https://openweathermap.org/img/wn/`+ jsObject.weather[0].icon +`.png`;
+        const desc = jsObject.weather[0].description;
+        document.getElementById(`imagesrc`).textContent = imagesrc;
+        document.getElementById(`icon`).setAttribute('src',imagesrc);
+        document.getElementById(`icon`).setAttribute(`alt`, desc);
     });
